b4c7fb5b8ab9260ffa5f51b299d2759379d3848a
Bodigam Kartheek Reddy <bodigam.10849697@ltimindtree.com>
2026-08-04 15:43:23 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DotnetSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("🚀 Hello GitHub Actions + Terraform!");
+        }
+    }
+}
